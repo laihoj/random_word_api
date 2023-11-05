d6a169f36b98bbdb310365839e629080b8e44a24
laihoj <jaakkolaiho94@gmail.com>
2023-11-05 17:54:36 +0100

    parse word list

--- api.js
+++ api.js
@@ -3,6 +3,20 @@ exports.handler = async (event, context) => {
     if (event.httpMethod === 'GET') {
       try {
         // Process the GET request as needed
+        const fs = require("fs");
+        const { parse } = require("csv-parse");
+        fs.createReadStream("./nykysuomensanalista2022.csv")
+        .pipe(parse({ delimiter: ",", from_line: 2 }))
+        .on("data", function (row) {
+          console.log(row);
+        })
+        .on("error", function (error) {
+          console.log(error.message);
+        })
+        .on("end", function () {
+          console.log("finished");
+        });
+      
         //const data = require('./db.json');
   
         const helloWorld = {
